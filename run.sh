bddd62a2a377eeb3784f14f37be0a0f1312eed80
David Dean <ddean@ieee.org>
2022-09-03 16:02:20 +1000

    allow variable input

--- run.sh
+++ run.sh
@@ -1,20 +1,26 @@
 #!/usr/bin/env bash
 source .env
 
+INPUT="${1:-/to_upload}"
+IMPORT_PATH=$(mktemp -d)
+
+echo "Uploading videos from $INPUT"
+
 docker build https://github.com/mapillary/mapillary_tools.git#main:docker \
        -t mapillary_tools:latest
 
 docker run \
-       -v config.mapillary:/root/.config/mapillary \
-       -v to_upload:/to_upload \
+       -v "$(pwd)/config.mapillary:/root/.config/mapillary" \
+       -v "$(pwd)/to_upload:/to_upload" \
+       -v "$IMPORT_PATH:$IMPORT_PATH" \
        -e "PYTHONUNBUFFERED=1" \
        --env-file .env \
        mapillary_tools:latest \
        mapillary_tools --verbose \
-        video_process_and_upload /to_upload \
+        video_process_and_upload "$INPUT" "$IMPORT_PATH" \
         --user_name $MAPILLARY_USER \
         --geotag_source "blackvue_videos" \
-        --geotag_source_path /to_upload \
+        --geotag_source_path "$INPUT" \
         --interpolate_directions \
         --video_sample_interval 0.5 \
         --device_make "Blackvue" \
