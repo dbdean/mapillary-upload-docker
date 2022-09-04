aeb7af95cbe745be883e3e21dd66466256fd6f2c
David Dean <ddean@ieee.org>
2022-09-04 19:58:03 +1000

    changed to blakvue upload, rename already uploaded files

--- run.sh
+++ run.sh
@@ -4,11 +4,25 @@ source .env
 INPUT="${1:-/to_upload}"
 IMPORT_PATH=$(mktemp -d)
 
-echo "Uploading videos from $INPUT"
-
+# ensure docker image is built
 docker build https://github.com/mapillary/mapillary_tools.git#main:docker \
        -t mapillary_tools:latest
 
+# rename already uploaded files to done
+grep "Sending upload_end via IPC" $INPUT/upload.log \
+    | cut -f 8 -d ":" \
+    | cut -f 1 -d "," \
+    | cut -c 3- \
+    | rev \
+    | cut -c 2- \
+    | rev \
+    | xargs file \
+    | grep ISO \
+    | cut -d ':' -f1 \
+    | xargs -ti mv {} {}-done
+
+
+echo "Uploading videos from $INPUT (see $INPUT/upload.log)"
 docker run \
        -v "$(pwd)/config.mapillary:/root/.config/mapillary" \
        -v "$(pwd)/to_upload:/to_upload" \
@@ -17,15 +31,7 @@ docker run \
        --env-file .env \
        mapillary_tools:latest \
        mapillary_tools --verbose \
-        video_process_and_upload "$INPUT" "$IMPORT_PATH" \
+        upload_blackvue "$INPUT" \
         --user_name $MAPILLARY_USER \
-        --geotag_source "blackvue_videos" \
-        --geotag_source_path "$INPUT" \
-        --interpolate_directions \
-        --video_sample_interval 0.5 \
-        --device_make "Blackvue" \
-        --device_model "DR900S-1CH" \
-        --overwrite_EXIF_gps_tag \
-        --duplicate_distance 1.0 \
-        --skip_process_errors
+  2>&1 | tee -a $INPUT/upload.log
 
